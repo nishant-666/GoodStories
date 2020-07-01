37ad3c3473620bd2e70bc3f31771b1c92b0de2e9
nishant666 <nishants440@gmail.com>
2020-07-01 17:04:52 +0530

    v2

--- app.js
+++ app.js
@@ -38,6 +38,15 @@ if(process.env.NODE_ENV === 'developement'){
     app.use(morgan('dev'))
 }
 
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
 const { formatDate,stripTags,truncate,editIcon} = require('./helpers/hbs')
 
 app.engine('.hbs', exphbs({helpers:{
